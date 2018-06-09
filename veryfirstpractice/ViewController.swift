2e90fe6274b3f8dd898d33e3e7c44c8e225d32b3
Gokhan Gultekin <gokhangultekin07@hotmail.com>
2018-06-09 14:17:23 +0300

    A second commit.
    Second commit before creating new feature branch.

--- veryfirstpractice/ViewController.swift
+++ veryfirstpractice/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var countries = [String]()
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        countries = ["Sweden","Netherlands","Germany","Denmark","Norway"]
+        
     }
 
 
