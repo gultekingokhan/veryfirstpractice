028db67521703b471713232f52824cec95223265
Gokhan Gultekin <gokhangultekin07@hotmail.com>
2018-06-09 15:18:15 +0300

    Demo app has been created.
    First commit of the very first branch. In this commit:
    - I deleted the storyboard and instantiated app from scratch as you can see in AppDelegate.Swift.
    - Created a table view which lists countries array.

---
+++ veryfirstpractice/CountryCell/CountryCell.swift
@@ -0,0 +1,25 @@
+//
+//  CountryCell.swift
+//  veryfirstpractice
+//
+//  Created by Gokhan Gultekin on 9.06.2018.
+//  Copyright © 2018 Gokhan. All rights reserved.
+//
+
+import UIKit
+
+class CountryCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- veryfirstpractice/ViewController.swift
+++ veryfirstpractice/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    var countries = [String]()
+    var countries: [String] = ["Sweden","Netherlands","Germany","Denmark","Norway"]
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableView: UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        countries = ["Sweden","Netherlands","Germany","Denmark","Norway"]
+
+        let nib = UINib(nibName: "CountryCell", bundle: nil)
+        tableView?.register(nib, forCellReuseIdentifier: "CountryCell")
         
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell", for: indexPath) as? CountryCell {
+            
+            let countryName = countries[indexPath.row]
+            cell.nameLabel.text = countryName
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
 }
 
